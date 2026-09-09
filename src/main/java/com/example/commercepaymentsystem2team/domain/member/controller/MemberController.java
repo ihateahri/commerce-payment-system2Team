106005b67e737a7991a9b4d00c57f59f7a0af011
wsungan <ws960221@gmail.com>
2026-09-09 21:09:17 +0900

    domain - member controller/service/repository/dto 추가

--- src/main/java/com/example/commercepaymentsystem2team/domain/member/controller/MemberController.java
+++ src/main/java/com/example/commercepaymentsystem2team/domain/member/controller/MemberController.java
@@ -1,4 +1,23 @@
 package com.example.commercepaymentsystem2team.domain.member.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.example.commercepaymentsystem2team.domain.member.dto.MemberResponse;
+import com.example.commercepaymentsystem2team.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    //내 정보 조회
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponse> me(@AuthenticationPrincipal Long memberId) {
+        return ResponseEntity.ok(memberService.getMe(memberId));
+    }
 }
