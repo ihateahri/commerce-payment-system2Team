931fc424ce3f6f692ef3a8d6d30ad375ba2420bc
ihateahri <ihateahri@naver.com>
2026-09-11 19:31:00 +0900

    feat: Refund 엔티티 , RefundRepository 구현

---
+++ src/main/java/com/example/commercepaymentsystem2team/domain/refund/entity/Refund.java
@@ -0,0 +1,40 @@
+package com.example.commercepaymentsystem2team.domain.refund.entity;
+
+import com.example.commercepaymentsystem2team.common.entity.BaseEntity;
+import com.example.commercepaymentsystem2team.domain.payment.entity.Payment;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "refund")
+public class Refund extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "payment_id", nullable = false)
+    private Payment payment;
+
+    @Column(nullable = false, length = 100)
+    private String reason;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RefundStatus status;
+
+    public Refund(Payment payment, String reason, Long amount) {
+        this.payment = payment;
+        this.reason = reason;
+        this.amount = amount;
+        this.status = RefundStatus.COMPLETED;
+    }
+}
