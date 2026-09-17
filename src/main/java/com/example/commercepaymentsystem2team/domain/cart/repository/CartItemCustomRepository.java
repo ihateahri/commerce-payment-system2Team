ecfef38d4d276e968eb3977f892341188320b665
ihateahri <ihateahri@naver.com>
2026-09-17 12:35:27 +0900

    feat: CartItemRepository QueryDsl적용

---
+++ src/main/java/com/example/commercepaymentsystem2team/domain/cart/repository/CartItemCustomRepository.java
@@ -0,0 +1,9 @@
+package com.example.commercepaymentsystem2team.domain.cart.repository;
+
+import com.example.commercepaymentsystem2team.domain.cart.entity.CartItem;
+
+import java.util.List;
+
+public interface CartItemCustomRepository {
+    List<CartItem> findAllByMemberId(Long memberId);
+}
