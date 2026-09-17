44d8477bbb486933284c85af3b24e06297ed7e87
ihateahri <ihateahri@naver.com>
2026-09-17 13:04:01 +0900

    fix: ProductRepository 오류수정

--- src/main/java/com/example/commercepaymentsystem2team/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/commercepaymentsystem2team/domain/product/repository/ProductRepository.java
@@ -3,14 +3,13 @@ package com.example.commercepaymentsystem2team.domain.product.repository;
 import com.example.commercepaymentsystem2team.domain.product.entity.Product;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductCustomRepository {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT p FROM Product p WHERE p.id = :id")
