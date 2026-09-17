e4b0f304727ce92c240c4c56a0ae717aff3feb5f
ihateahri <ihateahri@naver.com>
2026-09-17 14:12:22 +0900

    fix: ProductRepository 수정 및 각 repository 불필요한 import 삭제

--- src/main/java/com/example/commercepaymentsystem2team/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/commercepaymentsystem2team/domain/order/repository/OrderRepository.java
@@ -2,8 +2,6 @@ package com.example.commercepaymentsystem2team.domain.order.repository;
 
 import com.example.commercepaymentsystem2team.domain.order.entity.Order;
 import jakarta.persistence.LockModeType;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
